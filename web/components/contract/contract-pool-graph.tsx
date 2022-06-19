ee9d3c6084c5787ca8743bc1c09c3707adc23ecc
Gurkenglas <gurkenglas@hotmail.de>
2022-06-19 22:23:32 +0200

    symmetric liquidity metric

--- web/components/contract/contract-pool-graph.tsx
+++ web/components/contract/contract-pool-graph.tsx
@@ -1,4 +1,4 @@
-import { ResponsiveStream } from '@nivo/stream'
+import { ResponsiveLine } from '@nivo/line'
 import { CPMMContract } from 'common/contract'
 import { useWindowSize } from 'web/hooks/use-window-size'
 
@@ -14,32 +14,33 @@ export const ContractPoolGraph = function ContractPoolGraph(props: {
   const { contract, height } = props
   const { p, pool } = contract
   const k = pool.YES ** p + pool.NO ** (1 - p)
-  const data = Array.from({ length: 99 }, (_, i) => ({
-    YES: getShares((99 - i) / 100, k, 1 - p),
-    NO: getShares((i + 1) / 100, k, p),
+  const points = Array.from({ length: 99 }, (_, i) => ({
+    x: i + 1,
+    y:
+      getShares((99 - i) / 100, k, 1 - p) * p +
+      getShares((i + 1) / 100, k, p) * (1 - p),
   }))
+  const data = [{ id: 'Yes', data: points, color: '#11b981' }]
   const { width } = useWindowSize()
   return (
     <div
       className="w-full overflow-visible"
       style={{ height: height ?? ((width ?? 0) >= 800 ? 350 : 250) }}
     >
-      <ResponsiveStream
+      <ResponsiveLine
         data={data}
-        keys={['YES', 'NO']}
         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
         axisBottom={{
-          tickValues: [],
+          tickValues: [1, 20, 40, 60, 80, 99],
           legend: 'market price in [1,99]%',
           legendOffset: 30,
         }}
         axisLeft={{
-          legend: 'shares that would be in the liquidity pool',
+          legend:
+            'shares that someone has to buy to move the market price by 1%',
           legendOffset: -40,
         }}
         enableGridY={false}
-        offsetType="diverging"
-        colors={['green', 'red']}
         legends={[
           {
             anchor: 'bottom-right',
