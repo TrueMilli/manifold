3fadff817f71dec4fc282e4f85724e5125af5424
Gurkenglas <gurkenglas@hotmail.de>
2022-06-19 15:35:45 +0200

    resizeobserverbug

--- web/components/contract/contract-pool-graph.tsx
+++ web/components/contract/contract-pool-graph.tsx
@@ -6,10 +6,9 @@ import { getInitialProbability } from 'common/calculate'
 import { CPMMContract } from 'common/contract'
 import { useWindowSize } from 'web/hooks/use-window-size'
 
-function getShares(prob: number, k: number, p: number) {
-  return (
-    (((((1 - p) / p) * prob) / (1 - prob)) * k ** (1 / p)) ** (p / (2 * p - 1))
-  )
+function getShares(prob: number, l: number, p: number) {
+  return Math.exp(l + p * (Math.log(p) + Math.log(1-p) + Math.log(1-prob)))
+  //return ((((((1 - p) / p) * prob) / (1 - prob)) * k ** (1 / p)) ** (p / (2 * p - 1)))
 }
 
 export const ContractPoolGraph = memo(function ContractProbGraph(props: {
@@ -19,110 +18,19 @@ export const ContractPoolGraph = memo(function ContractProbGraph(props: {
 }) {
   const { contract, height } = props
   const { p, pool } = contract
-
-  const buckets = Array.from(Array(101).keys())
-  const distribution = Object.assign(
-    {},
-    ...buckets.map((prob) => ({ [prob]: getShares(prob, k, p) }))
-  )
-
-  const k = pool.YES ** p * pool.NO ** (1 - p)
-  const bets = props.bets.filter((bet) => !bet.isAnte && !bet.isRedemption)
-
-  const startProb = getInitialProbability(contract)
-
-  const probs = [startProb, ...bets.map((bet) => bet.probAfter)]
-
-  // Add a fake datapoint so the line continues to the right
-  probs.push(probs[probs.length - 1])
-
-  const yTickValues = [0, 25, 50, 75, 100]
+  const l = Math.log(pool.YES) * p + Math.log(pool.NO) * (1 - p)
+  const distribution = Array.from({ length: 101 }, (_, prob) => ({Foo:getShares(prob,l,p)}))
 
   const { width } = useWindowSize()
-
   const numXTickValues = !width || width < 800 ? 2 : 5
-  const data = [
-    {
-      Raoul: 42,
-      Josiane: 190,
-      Marcel: 137,
-      René: 93,
-      Paul: 75,
-      Jacques: 11,
-    },
-    {
-      Raoul: 119,
-      Josiane: 84,
-      Marcel: 46,
-      René: 69,
-      Paul: 167,
-      Jacques: 11,
-    },
-    {
-      Raoul: 101,
-      Josiane: 79,
-      Marcel: 22,
-      René: 63,
-      Paul: 105,
-      Jacques: 192,
-    },
-    {
-      Raoul: 165,
-      Josiane: 70,
-      Marcel: 57,
-      René: 30,
-      Paul: 82,
-      Jacques: 46,
-    },
-    {
-      Raoul: 147,
-      Josiane: 86,
-      Marcel: 87,
-      René: 60,
-      Paul: 59,
-      Jacques: 104,
-    },
-    {
-      Raoul: 11,
-      Josiane: 195,
-      Marcel: 140,
-      René: 149,
-      Paul: 98,
-      Jacques: 131,
-    },
-    {
-      Raoul: 73,
-      Josiane: 159,
-      Marcel: 125,
-      René: 43,
-      Paul: 49,
-      Jacques: 70,
-    },
-    {
-      Raoul: 168,
-      Josiane: 136,
-      Marcel: 199,
-      René: 174,
-      Paul: 121,
-      Jacques: 189,
-    },
-    {
-      Raoul: 151,
-      Josiane: 170,
-      Marcel: 138,
-      René: 177,
-      Paul: 71,
-      Jacques: 52,
-    },
-  ]
   return (
     <div
       className="w-full overflow-visible"
       style={{ height: height ?? (!width || width >= 800 ? 350 : 250) }}
     >
       <ResponsiveStream
-        data={data}
-        keys={['Raoul', 'Josiane', 'Marcel', 'René', 'Paul', 'Jacques']}
+        data={distribution}
+        keys={['Foo']}
         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
         axisTop={null}
         axisRight={null}
@@ -146,40 +54,6 @@ export const ContractPoolGraph = memo(function ContractProbGraph(props: {
         colors={{ scheme: 'nivo' }}
         fillOpacity={0.85}
         borderColor={{ theme: 'background' }}
-        defs={[
-          {
-            id: 'dots',
-            type: 'patternDots',
-            background: 'inherit',
-            color: '#2c998f',
-            size: 4,
-            padding: 2,
-            stagger: true,
-          },
-          {
-            id: 'squares',
-            type: 'patternSquares',
-            background: 'inherit',
-            color: '#e4c912',
-            size: 6,
-            padding: 2,
-            stagger: true,
-          },
-        ]}
-        fill={[
-          {
-            match: {
-              id: 'Paul',
-            },
-            id: 'dots',
-          },
-          {
-            match: {
-              id: 'Marcel',
-            },
-            id: 'squares',
-          },
-        ]}
         dotSize={8}
         dotColor={{ from: 'color' }}
         dotBorderWidth={2}
